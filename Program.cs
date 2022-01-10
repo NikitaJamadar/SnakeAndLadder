9e5d4666dfa68e28693050b594d019a262b2fb3d
nikhita <nikhitajamadar1998@gmail.com>
2022-01-09 19:25:58 -0800

    displying welcome to snake and ladder program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder program");
+            //UC1.playerAtPositionZero();
+            //UC2.RollTheDice();
+            //UC3.CheckingPosition();
+            //UC4.RepeatTillWin();
+            //UC5.RepeatTillWinningPosition();
+            UC6.CountRollDice();
+
+        }
+    }
+}
