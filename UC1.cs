a49b049b76d5bc0810ccba362c1960057e046b9b
nikhita <nikhitajamadar1998@gmail.com>
2022-01-09 19:29:40 -0800

    showing the player at zero position

---
+++ UC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class UC1
+    {
+        public static void playerAtPositionZero()
+        {
+            int player = 1;
+            int startingPosition = 0;
+            Console.WriteLine("Number of players="+player);
+            Console.WriteLine("position at start=" + startingPosition);
+
+        }
+        
+    }
+}
