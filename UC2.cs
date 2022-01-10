5d6d739cbfc82f4f3f0780b405c7e4e322317b81
nikhita <nikhitajamadar1998@gmail.com>
2022-01-09 19:33:23 -0800

    Rolling the dice

---
+++ UC2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class UC2
+    {
+        public static void RollTheDice()
+        {
+            int player = 1;
+            int StartingPositon = 0;
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+            Console.WriteLine("No. of Player " + player);
+
+            Console.WriteLine("You got number :" + DiceRoll);
+            Console.ReadLine();
+        }
+    }
+}
